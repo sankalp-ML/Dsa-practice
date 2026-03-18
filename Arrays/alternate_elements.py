0747de21d574058205f614389837b67848b7de8c
Sankalp <sankalphv8088@gmail.com>
2026-03-18 17:48:17 +0530

    COMMIT problem solved

---
+++ Arrays/alternate_elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getAlternates(self, arr):
+        # Code Here
+        m=[]
+        for i in range(len(arr)):
+            if i%2==0:
+                m.append(arr[i])
+        return m
+    
+
+c=Solution()
+arr=[1, 2, 3, 4]
+print(c.getAlternates(arr))
+
+
+#direct method
+print(arr[::2])
